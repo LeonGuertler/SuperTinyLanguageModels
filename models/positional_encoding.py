44d156210497e3e9b0459beb1da1693e425123eb
Leon <leon@system76-pc.localdomain>
2024-04-02 18:56:03 +0200

    Re-write

--- models/positional_encoding.py
+++ models/positional_encoding.py
@@ -23,7 +23,7 @@ class LearnedPosEncoding(nn.Module):
         """
         print(x.device)
         print(self.pe.weight.device)
-        print(self.pe(torch.arange(x.size(1)-5, device=x.device)))
+        print(self.pe(torch.arange(x.size(1)-1, device=x.device)))
         x = x.to('cpu')
         self.pe.weight = self.pe.weight.to('cpu')
         if len(x.shape) >= 2:
