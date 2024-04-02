62968f9644d175e90d4ca1e63d9a8191f64673b8
Leon <leon@system76-pc.localdomain>
2024-04-02 18:34:45 +0200

    Re-write

--- models/positional_encoding.py
+++ models/positional_encoding.py
@@ -21,12 +21,7 @@ class LearnedPosEncoding(nn.Module):
         """
         Forward pass
         """
-        print(x.size())
-        print(x.device)
-        print(torch.arange(x.size(1)))
-        #print(torch.arange(x.size(1), device=x.device))
         if len(x.shape) >= 2:
-            return self.pe(torch.arange(x.size(1))).unsqueeze(0)
-            return self.pe(torch.arange(x.size(1), device=x.device)).unsqueeze(0)
+            return self.pe(torch.arange(x.size(1))).unsqueeze(0).to(x.device)
         else:
             return self.pe(torch.arange(x.size(1), device=x.device))
