4f73f3b84c9f1ae8afc5aa59e8560312b9c1de8d
Dylan Hillier <dylanslavinhillier@gmail.com>
2024-04-04 14:33:47 +0800

    adds back old eval code

---
+++ eval/hellaswag.py
@@ -0,0 +1,75 @@
+"""Hella Swag Benchmark Code: https://arxiv.org/pdf/1905.07830.pdf"""
+
+import tqdm
+
+from datasets import load_dataset
+
+from evals import benchmark
+
+HELLA_SWAG_PROMPT = """Your task is to pick the most plausible continuation of a story
+Example:
+Story: John went to the store. He bought some milk.
+Options:
+A: He went home.
+B: He went to the park.
+C: He went to the moon.
+D: He went to work.
+Answer: A
+
+Story: {story}
+Options:
+A: {option1}
+B: {option2}
+C: {option3}
+D: {option4}
+Answer: """
+
+REMAP = {
+    0: "A",
+    1: "B",
+    2: "C",
+    3: "D",
+}
+
+
+class HellaSwag(benchmark.Benchmark):
+    """HellaSwag benchmark"""
+
+    def __init__(self, name, model, cache_dir="data/eval/hellaswag"):
+        super().__init__(name, model)
+        self.base_dataset = load_dataset("Rowan/hellaswag", cache_dir=cache_dir)[
+            "validation"
+        ]
+        # preprocess the dataset
+
+    def execute(self, batch_size=8):
+        acc_metric = benchmark.AccuracyMetric()
+        f1_metric = benchmark.F1Metric()
+        # batch together samples for inference
+        batch_prompts = []
+        batch_labels = []
+        for sample in tqdm.tqdm(self.base_dataset):
+            options = sample["endings"]
+            prompt = HELLA_SWAG_PROMPT.format(
+                story=sample["ctx"],
+                option1=options[0],
+                option2=options[1],
+                option3=options[2],
+                option4=options[3],
+            )
+            label = REMAP[int(sample["label"])]
+            batch_prompts.append(prompt)
+            batch_labels.append(label)
+            if len(batch_prompts) == batch_size:
+                predictions = self.model.predict(batch_prompts, options=["A", "B"])
+                targets = batch_labels
+                acc_metric.batched_accumulate(predictions, targets)
+                f1_metric.batched_accumulate(predictions, targets)
+                batch_prompts = []
+                batch_labels = []
+        return acc_metric.aggregate(), f1_metric.aggregate()
+
+
+if __name__ == "__main__":
+    hellaswag = HellaSwag("hellaswag", benchmark.FauxModel())
+    print(hellaswag.execute())
---
+++ eval/nonsense.py
@@ -0,0 +1,42 @@
+"""Nonsense Benchmark: https://github.com/google/BIG-bench/tree/main/bigbench/benchmark_tasks/nonsense_words_grammar"""
+
+import tqdm
+
+from datasets import load_dataset
+
+from evals import benchmark
+
+
+class Nonsense(benchmark.Benchmark):
+    """Nonsense benchmark from bigbench"""
+
+    def __init__(self, name, model, cache_dir="data/eval/nonsense"):
+        super().__init__(name, model)
+        self.base_dataset = load_dataset("tasksource/bigbench", "nonsense_words_grammar", cache_dir=cache_dir)["validation"]
+        # preprocess the dataset
+
+    def execute(self, batch_size=8):
+        acc_metric = benchmark.AccuracyMetric()
+        # batch together samples for inference
+        batch_prompts = []
+        batch_labels = []
+        output_tokens = []
+        for sample in tqdm.tqdm(self.base_dataset):
+            prompt = sample["inputs"]
+            label = str(sample["targets"][0])
+            batch_prompts.append(prompt)
+            batch_labels.append(label)
+            output_tokens.append(sample["multiple_choice_targets"])
+            if len(batch_prompts) == batch_size:
+                predictions = self.model.predict(batch_prompts, options=output_tokens)
+                targets = batch_labels
+                acc_metric.batched_accumulate(predictions, targets)
+                batch_prompts = []
+                batch_labels = []
+                output_tokens = []
+        return acc_metric.aggregate()
+
+
+if __name__ == "__main__":
+    arc = Nonsense("nonsense", benchmark.FauxModel())
+    print(arc.execute())
