1e6fb92667fc9d16d362e044d331bec75314e8e6
LeonGuertler <leon@system76-pc.localdomain>
2024-04-16 07:24:30 +0200

    added seq2seq (still buggy)

--- trainers/dataloader.py
+++ trainers/dataloader.py
@@ -153,7 +153,7 @@ class Seq2SeqDataloader:
             os.path.join(self.dataset_path, f"{split}.bin"), dtype=np.uint16, mode="r"
         )
 
-        ix = torch.randint(len(data) - self.context_window, (self.batch_size,))
+        ix = torch.randint(len(data) - 2*self.context_window, (self.batch_size,))
         X = torch.stack(
             [
                 torch.from_numpy((data[i : i + self.context_window]).astype(np.int64))
