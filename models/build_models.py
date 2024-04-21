6d8624d3c53ff30dc2b1dbb3e19d3a8f20233114
LeonGuertler <lg11@outlook.com>
2024-04-21 13:49:38 +0200

    debugging and accelerating bpe tokenizer

--- models/build_models.py
+++ models/build_models.py
@@ -7,7 +7,6 @@ from models.architectures.baseline import BaseGPT
 from models.architectures.profiler import ProfilerGPT
 from models.architectures.shared_ffn import SharedFFN
 from models.architectures.shared_ffn_lora import SharedFNNLora
-from models.architectures.seq2seq_transformer import Seq2SeqModel
 
 
 
@@ -16,7 +15,6 @@ MODEL_CLASSES = {
     "profiler": ProfilerGPT,
     "sharedfnn": SharedFFN,
     "sharedfnnlora": SharedFNNLora,
-    "seq2seq_baseline": Seq2SeqModel
 }
 
 
