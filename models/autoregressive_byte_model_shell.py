c5271e6d420d97e18325f36077ab8211cac1101c
LeonGuertler <lg11@outlook.com>
2024-04-25 07:00:43 +0200

    bug fixed (hopefully)

--- models/autoregressive_byte_model_shell.py
+++ models/autoregressive_byte_model_shell.py
@@ -208,7 +208,7 @@ class ByteLevelProcessor(nn.Module):
                 print(token_string)
                 byte_ids = self.byte_tokenizer.encode(token_string)
                 # convert to tensor
-                byte_ids = torch.tensor(byte_ids).to('cuda').un
+                byte_ids = torch.tensor(byte_ids).to('cuda')
                 input(byte_ids)
                 # embed
                 x = self.token_embedder(byte_ids)#.unsqueeze(0)
