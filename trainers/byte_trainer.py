1b35c11d3fe2aa94bcf7046dbe38501b33975def
LeonGuertler <lg11@outlook.com>
2024-04-26 23:14:31 +0200

    substantial speed up (hopefully haha)

--- trainers/byte_trainer.py
+++ trainers/byte_trainer.py
@@ -83,8 +83,8 @@ class ByteTrainer:
         print("Preprocessing the training data")
 
         self.dataloader.prepare_data(
-            byte_tokenizer=self.model.pooling_tokenizer, 
-            pooling_tokenizer=self.model.byte_tokenizer,
+            byte_tokenizer=self.model.byte_tokenizer, 
+            pooling_tokenizer=self.model.pooling_tokenizer,
         )
 
     @torch.no_grad()
