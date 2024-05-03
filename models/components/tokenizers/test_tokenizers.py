99f4df1bace21cc2776c1a7d0b22776915b728d9
Dylan Hillier <dylanslavinhillier@gmail.com>
2024-05-03 14:56:48 +0800

    improves tests

--- models/components/tokenizers/test_tokenizers.py
+++ models/components/tokenizers/test_tokenizers.py
@@ -11,6 +11,8 @@ from models.components.tokenizers.gpt2_tokenizer import GPT2Tokenizer
 
 # test the GPT2tokenizer
 TEST_STRING = "This is a test string. <|endoftext|>"
+TEST_STRING_2 = "This is a test string."
+TEST_STRING_3 = "test"
 
 
 def test_gpt2_tokenizer():
@@ -18,6 +20,14 @@ def test_gpt2_tokenizer():
     tokens = tokenizer.encode(TEST_STRING)
     decoded = tokenizer.decode(tokens)
     assert decoded == TEST_STRING
+
+    tokens_list = tokenizer.encode_batch([TEST_STRING, TEST_STRING_2, TEST_STRING_3])
+    decoded_list = tokenizer.decode_batch(tokens_list)
+    assert decoded_list[0] == TEST_STRING
+    assert decoded_list[1] == TEST_STRING_2
+
+    tensor, mask = tokenizer.pad_batch(tokens_list)
+    assert tensor.shape == mask.shape
 
 
 def test_bpe_tokenizer():
@@ -30,9 +40,13 @@ def test_bpe_tokenizer():
     tokenizer = BPETokenizer(vocab_size=512, dataset_name="debug")
     tokens = tokenizer.encode(TEST_STRING)
     decoded = tokenizer.decode(tokens)
-    print(tokens)
-    print(decoded)
     assert decoded == TEST_STRING
 
-    print(tokenizer.special_tokens["<|endoftext|>"])
+    tokens_list = tokenizer.encode_batch([TEST_STRING, TEST_STRING_2, TEST_STRING_3])
+    decoded_list = tokenizer.decode_batch(tokens_list)
+    assert decoded_list[0] == TEST_STRING
+    assert decoded_list[1] == TEST_STRING_2
+
+    tensor, mask = tokenizer.pad_batch(tokens_list)
+    assert tensor.shape == mask.shape
     # tokenizer.vocab[tokenizer.special_tokens["<|endoftext|>"]]
