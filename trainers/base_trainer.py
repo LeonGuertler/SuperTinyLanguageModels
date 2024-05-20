6d5fdbb8c468da27c04cbe660722d088f97d6a47
Dylan Hillier <dylanslavinhillier@gmail.com>
2024-05-20 17:04:52 +0800

    undoes some commenting (this should be sorted elsewhere)

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -106,17 +106,17 @@ class BaseTrainer:
             perplexities = torch.zeros(eval_iters)
             for i in range(eval_iters):
                 x, y = self.dataloader.get_batch(split)
-                #decoded_xs = [tokenizer.decode(x[i].tolist()) for i in range(x.size(0))]
-                #token_lengths = [x.size(1) for _ in range(x.size(0))]
-                #char_lengths = [len(decoded_x) for decoded_x in decoded_xs]
+                decoded_xs = [tokenizer.decode(x[i].tolist()) for i in range(x.size(0))]
+                token_lengths = [x.size(1) for _ in range(x.size(0))]
+                char_lengths = [len(decoded_x) for decoded_x in decoded_xs]
                 with self.ctx:
                     output, _ = self.model(x)
                     losses[i] = self.loss_fn(output, y)
-                    #perplexities[i] = torch.exp(
-                    #    losses[i] * sum(token_lengths) / sum(char_lengths)
-                    #)
+                    perplexities[i] = torch.exp(
+                        losses[i] * sum(token_lengths) / sum(char_lengths)
+                    )
             loss[split] = losses.mean().item()
-            #perplexity[split] = perplexities.mean().item()
+            perplexity[split] = perplexities.mean().item()
         self.model.train()
         return loss, perplexity
 
