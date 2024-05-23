155b0f47d7ed8724638aee34200cf291b46c64bf
LeonGuertler <lg11@outlook.com>
2024-05-23 14:29:42 +0700

    perpelexity debugging

--- trainers/loss_fn.py
+++ trainers/loss_fn.py
@@ -15,8 +15,8 @@ def cross_entropy_loss_fn(logits, y, mask=None):
         logits = logits.view(B, S*S_c, -1)
         y = y.view(B, S*S_c)
     if mask is not None:
-        logits = logits.view(-1, logits.size(-1))[mask]
-        y = y.view(-1)[mask]
+        logits = logits[mask]
+        y = y[mask]
     else:
         logits = logits.view(-1, logits.size(-1))
         y = y.view(-1)
