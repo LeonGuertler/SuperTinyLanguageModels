e6f654047334dff9aeba25c1b5cbfc8fff65f85a
LeonGuertler <lg11@outlook.com>
2024-08-01 02:30:46 +0700

    initial mean pooling with stride=4

--- trainers/datasets.py
+++ trainers/datasets.py
@@ -96,7 +96,7 @@ class BytePatchDataset(DatasetInterface):
         and return every fourth token as a label
         (since they will be pooled anyway)
         """
-        x = torch.from_numpy((self.data[idx: idx + 4*self.context_window:4]).astype(np.int64))
+        x = torch.from_numpy((self.data[idx: idx + 4*self.context_window]).astype(np.int64))
         y = torch.from_numpy((self.data[idx + 4: idx + 4 + 4*self.context_window:4]).astype(np.int64))
         print(x.size(), y.size())
         return x, y
