f2f1742cbd177403abd04e51dd7b02cf02a25e10
LeonGuertler <lg11@outlook.com>
2024-08-14 13:17:37 +0700

    testing

--- evals/text_modeling/text_modeling_evaluator.py
+++ evals/text_modeling/text_modeling_evaluator.py
@@ -57,7 +57,7 @@ class TextModelingEvaluator(EvaluationInterface):
         """
         Process a chunk of text by predicting the next word after the chunk.
         """
-        inputs = self.model.embedding_model.tokenize_input(chunk, return_tensors="pt")
+        inputs = self.model.embedding_model.tokenize_input(chunk) #, return_tensors="pt")
         input_ids = inputs.input_ids
 
         # Get logits from the model (normal forward pass)
