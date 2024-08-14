a047cd12dde935d463af7016d3b2c106602495f1
LeonGuertler <lg11@outlook.com>
2024-08-14 17:19:23 +0700

    fix multi-gpu

--- models/components/tokenizers/setup.py
+++ models/components/tokenizers/setup.py
@@ -5,12 +5,14 @@ A script for building the various tokenizers.
 from models.components.tokenizers.base_class import Tokenizer
 from models.components.tokenizers.bpe import BPETokenizer
 from models.components.tokenizers.gpt2 import GPT2Tokenizer
+from models.components.tokenizers.ck100k import CL100KTokenizer
 
 TOKENIZER_DICT = {
     "gpt2": lambda vocab_size, dataset_name: GPT2Tokenizer(),
     "bpe": lambda vocab_size, dataset_name: BPETokenizer(
         vocab_size=vocab_size, dataset_name=dataset_name
     ),
+    "cl100k": lambda vocab_size, dataset_name: CL100KTokenizer(),
 }
 
 
