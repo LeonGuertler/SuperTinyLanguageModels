4e8e461b44a675e766009f014928ef87a7cc069a
LeonGuertler <lg11@outlook.com>
2024-08-16 20:41:39 +0700

    added byte level accuracy and byte-level perplexity

--- models/components/tokenizers/llama_30k.py
+++ models/components/tokenizers/llama_30k.py
@@ -3,27 +3,23 @@ A simple wrapper around the LLaMA Tokenizer to
 standardize the interface for tokenization.
 """
 
-import sentencepiece as spm
-import torch
-from huggingface_hub import hf_hub_download
-from models.components.tokenizers.base_class import Tokenizer
+
+from transformers import AutoTokenizer
 
 class LLaMATokenizer(Tokenizer):
-    """A simple wrapper around the LLaMA Tokenizer."""
+    """A simple wrapper around a LLaMA-based Tokenizer."""
 
     def __init__(self, **_):
         super().__init__()
-        # Hardcoded model path from Hugging Face
-        model_name_or_path = "decapoda-research/llama-7b-hf"
-        model_path = hf_hub_download(repo_id=model_name_or_path, filename="tokenizer.model")
-        self.tokenizer = spm.SentencePieceProcessor(model_file=model_path)
-        self.eot_token = self.tokenizer.eos_id()
-        self.pad_token = self.tokenizer.pad_id()
-        self.vocab_size = self.tokenizer.get_piece_size()
+        model_name_or_path = "chavinlo/alpaca-native"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        self.eot_token = self.tokenizer.eos_token_id
+        self.pad_token = self.tokenizer.pad_token_id
+        self.vocab_size = self.tokenizer.vocab_size
 
     def encode(self, text):
         """Encode a string into tokens."""
-        return self.tokenizer.encode(text, out_type=int)
+        return self.tokenizer.encode(text, add_special_tokens=False)
 
     def encode_batch(self, texts):
         """Encode a list of strings into tokens."""
@@ -31,7 +27,6 @@ class LLaMATokenizer(Tokenizer):
 
     def decode(self, tokens):
         """Decode a list of tokens into a string."""
-        # check if the tokens are a tensor
         if torch.is_tensor(tokens):
             tokens = tokens.tolist()
         return self.tokenizer.decode(tokens)
