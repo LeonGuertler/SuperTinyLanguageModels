161572919bda8fcfc723cf09a4c32650ccde7f53
bobbycxy <bobbycxy1994@gmail.com>
2024-09-09 15:07:53 +0000

    bug fixes, but unoptimized

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -237,56 +237,74 @@ class BaseTrainer:
                             teacher_output, _ = self.teacher_model(x)
                             
                             ## attention
-                            # get the Q and K matrices as inputs are passed through the teacher model
-                            teacher_QKs = get_qk_scores(self.teacher_model, x)
-                            # get the pre-normalized attention matrices
-                            teacher_attns = get_prenormalized_attention_list(teacher_QKs, teacher_model=self.teacher_model)
+                            if self.kd_cfg['attn_loss_weight'] > 0.0:
+                                #
+                                # get the Q and K matrices as inputs are passed through the teacher model
+                                teacher_QKs = get_qk_scores(self.teacher_model, x)
+                                # get the pre-normalized attention matrices
+                                teacher_attns = get_prenormalized_attention_list(teacher_QKs, teacher_model=self.teacher_model)
 
                             ## hidden states
-                            teacher_hidden_states = self.teacher_model.core_model.hidden_states[1:]
+                            if self.kd_cfg['hs_loss_weight'] > 0.0:
+                                teacher_hidden_states = self.teacher_model.core_model.hidden_states[1:]
                             
                             ## embeddings
-                            teacher_embeddings = self.teacher_model.embedding_model.embedding_output
+                            if self.kd_cfg['embedding_loss_weight'] > 0.0:
+                                teacher_embeddings = self.teacher_model.embedding_model.embedding_output
 
                         ## get the student model output, with gradient calculation
                         student_output, aux_loss = self.DDP_model(x)
 
                         ## attention
-                        student_QKs = self.DDP_model.module.core_model.qk_lists
-                        student_attns = get_prenormalized_attention_list(student_QKs)
+                        if self.kd_cfg['attn_loss_weight'] > 0.0:
+                            student_QKs = self.DDP_model.module.core_model.qk_lists
+                            student_attns = get_prenormalized_attention_list(student_QKs)
+                            ## take every k-th attention matrix from teacher to match all students
+                            k = len(teacher_attns) // len(student_attns)
+                            teacher_attns = teacher_attns[::k] # get every k-th attention matrix
+
 
                         ## hidden states
-                        student_hidden_states = self.DDP_model.module.core_model.hidden_states
+                        if self.kd_cfg['hs_loss_weight'] > 0.0:
+                            student_hidden_states = self.DDP_model.module.core_model.hidden_states
+                            ## take every k-th attention matrix from teacher to match all students
+                            k = len(teacher_hidden_states) // len(student_hidden_states)
+                            teacher_hidden_states = teacher_hidden_states[::k] # get every k-th hidden state
+
 
                         ## embeddings
-                        student_embeddings = self.DDP_model.module.embedding_model.embedding_output
-                        # print(f"Student embeddings shape: {student_embeddings.shape}")
+                        if self.kd_cfg['embedding_loss_weight'] > 0.0:
+                            student_embeddings = self.DDP_model.module.embedding_model.embedding_output
+                            # print(f"Student embeddings shape: {student_embeddings.shape}")
                         
-                        ## take every k-th attention matrix from teacher to match all students
-                        k = len(teacher_attns) // len(student_attns)
-                        teacher_attns = teacher_attns[::k] # get every k-th attention matrix
-                        teacher_hidden_states = teacher_hidden_states[::k] # get every k-th hidden state
-
                         ## calculate the transformer's attention and hidden_state loss over each layer
                         attn_loss = 0.0
                         hs_loss = 0.0
-                        for i in range(len(student_attns)):
 
-                            ## project the student's hidden states to the teacher's dimensions
-                            if self.projection:
-                                student_hidden_states[i] = project_student_to_teacher_hs(self.projection, student_hidden_states[i])
+                        if self.kd_cfg['attn_loss_weight'] > 0.0 or self.kd_cfg['hs_loss_weight'] > 0.0:
 
-                            mean_student_attns = torch.mean(student_attns[i], dim=1) ## mean over the heads
-                            mean_teacher_attns = torch.mean(teacher_attns[i], dim=1) ## mean over the heads
-                            attn_loss += torch.nn.functional.mse_loss(mean_student_attns, mean_teacher_attns)
+                            for i in range(len(student_attns)):
+
+                                ## project the student's hidden states to the teacher's dimensions
+                                if self.projection and self.kd_cfg['hs_loss_weight'] > 0.0:
+                                    student_hidden_states[i] = project_student_to_teacher_hs(self.projection, student_hidden_states[i])
+
+                                    ## calculate the mean squared error loss between the student and teacher's attention and hidden states
+                                    hs_loss += torch.nn.functional.mse_loss(student_hidden_states[i], teacher_hidden_states[i])
+                                
+                                if self.kd_cfg['attn_loss_weight'] > 0.0:
+                                    ## calculate the mean squared error loss between the student and teacher's attention matrices
+                                    mean_student_attns = torch.mean(student_attns[i], dim=1) ## mean over the heads
+                                    mean_teacher_attns = torch.mean(teacher_attns[i], dim=1) ## mean over the heads
+                                    attn_loss += torch.nn.functional.mse_loss(mean_student_attns, mean_teacher_attns)
                             
-                            ## calculate the mean squared error loss between the student and teacher's attention and hidden states
-                            hs_loss += torch.nn.functional.mse_loss(student_hidden_states[i], teacher_hidden_states[i])
+                            
 
                         ## calculate the embeddings loss
-                        if self.projection:
+                        embeddings_loss = 0.0
+                        if self.projection and self.kd_cfg['embedding_loss_weight'] > 0.0:
                             student_embeddings = project_student_to_teacher_emb(self.projection, student_embeddings)
-                        embeddings_loss = torch.nn.functional.mse_loss(student_embeddings, teacher_embeddings)
+                            embeddings_loss = torch.nn.functional.mse_loss(student_embeddings, teacher_embeddings)
 
                         ## calculate the soft_targets loss
                         soft_targets_loss = self.distillation_loss_fn(student_output, teacher_output, self.kd_cfg['temperature'])
@@ -299,7 +317,7 @@ class BaseTrainer:
                         # print(f"Cross Entropy Loss: {label_loss.item()}")
 
                         ## combine the two losses
-                        print('embedding weight:', self.e, 'attn weight:', self.a, 'hs weight:', self.h, 'distil weight:', self.d, 'label weight:', self.l)
+                        # print('embedding weight:', self.e, 'attn weight:', self.a, 'hs weight:', self.h, 'distil weight:', self.d, 'label weight:', self.l)
                         loss = self.e*embeddings_loss + self.a*attn_loss + self.h*hs_loss + self.d*soft_targets_loss + self.l*label_loss
                         # loss = self.kd_cfg['distil_loss_weight']*soft_targets_loss + (1.0 - self.kd_cfg['distil_loss_weight'])*label_loss
                         # print(f"distil loss weight: {self.w}")
