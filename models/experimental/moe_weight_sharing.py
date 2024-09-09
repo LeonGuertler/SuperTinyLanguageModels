a70f819ee4aa945cc6bbd43663f927e7fd1c8e27
LeonGuertler <lg11@outlook.com>
2024-09-09 12:54:12 +0700

    added MoE sharing (debugging)

--- models/experimental/moe_weight_sharing.py
+++ models/experimental/moe_weight_sharing.py
@@ -66,7 +66,7 @@ class MoELoRA(torch.nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """ Forward pass """
         gate = torch.nn.functional.softmax(self.gate_linear(x), dim=-1)
-        
+        print(x.size())
         lora_contribution = torch.zeros_like(self.weight)
         for i in range(self.n_experts):
             expert_contribution = self.lora_experts_V[i] @ self.lora_experts_U[i]
