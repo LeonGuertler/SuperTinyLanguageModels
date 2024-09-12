289bcd4bb4ddee952cca15675695bc9ceba4d4a1
Dylan Hillier <dylanslavinhillier@gmail.com>
2024-09-12 12:39:48 +0800

    updates weight tying

--- models/experimental/weight_sharing.py
+++ models/experimental/weight_sharing.py
@@ -1,7 +1,5 @@
+import torch.nn as nn
 from models.core_models import GenericTransformer
-import torch
-from torch import nn
-# params: k_interior_layers, lora_rank
 
 class LoRA(nn.Module):
     def __init__(self, linear_layer, lora_rank):
@@ -14,7 +12,6 @@ class LoRA(nn.Module):
 
     def forward(self, x):
         """Forward pass through the linear layer with LoRA"""
-        # compute the LoRA weight matrix
         return self.linear_layer(x) + self.V(self.U(x))
 
 class SharedInteriorFFNLora(GenericTransformer):
@@ -22,71 +19,37 @@ class SharedInteriorFFNLora(GenericTransformer):
         super().__init__(model_cfg)
         self.k_interior_layers = model_cfg["k_interior_layers"]
         self.lora_rank = model_cfg["lora_rank"]
-        # share the weights between transformer blocks in layers 1+k_interior_layers to D-k_interior_layers
-        base_layer = 1 + self.k_interior_layers
-        ffn_0 = self.transformer.h[base_layer].ffn
-        shared_weights = {}
-        for name, module in ffn_0.named_modules():
-            if isinstance(module, torch.nn.Linear):
-                shared_weights[name] = module.weight
-        for i in range(1 + self.k_interior_layers, len(self.transformer.h) - self.k_interior_layers):
-            for name, module in self.transformer.h[i].ffn.named_modules():
-                if isinstance(module, torch.nn.Linear):
-                    module.weight = shared_weights[name]
-                    # wrap the linear layer with LoRA
-                    if self.lora_rank is not None:
-                        setattr(self.transformer.h[i].ffn, name, LoRA(module, self.lora_rank))
-
-
-
-                
         
+        self._apply_weight_sharing_and_lora(
+            start_layer=1 + self.k_interior_layers,
+            end_layer=len(self.transformer.h) - self.k_interior_layers,
+            module_name='ffn'
+        )
 
-                                                                             
+    def _apply_weight_sharing_and_lora(self, start_layer: int, end_layer: int, module_name: str):
+        base_module = getattr(self.transformer.h[start_layer], module_name)
+        shared_weights = {name: module.weight for name, module in base_module.named_modules() if isinstance(module, nn.Linear)}
+        
+        for i in range(start_layer, end_layer):
+            target_module = getattr(self.transformer.h[i], module_name)
+            for name, module in target_module.named_modules():
+                if isinstance(module, nn.Linear):
+                    module.weight = shared_weights[name]
+                    if self.lora_rank is not None:
+                        lora_module = LoRA(module, self.lora_rank)
+                        setattr(target_module, name, lora_module)
 
 
-# class GenericTransformer(torch.nn.Module):
-#     """
-#     Generic Transformer Class intended to be used for as
-#     broad a range of transformer models as possible.
-#     """
 
-#     def __init__(self, model_cfg):
-#         super().__init__()
+class SharedInteriorFFNLoraAndCProj(SharedInteriorFFNLora):
+    def __init__(self, model_cfg):
+        super().__init__(model_cfg)
 
-#         # build the transformer
-#         self.transformer = torch.nn.ModuleDict(
-#             {
-#                 "drop": torch.nn.Dropout(),
-#                 "h": torch.nn.ModuleList(
-#                     [
-#                         GenericTransformerBlock(
-#                             hidden_dim=model_cfg["hidden_dim"],
-#                             context_window=model_cfg["context_window"],
-#                             use_rope=model_cfg["positional_encoding_type"] == "rope",
-#                             ffn_cfg=model_cfg["core_model"]["ffn"],
-#                             attn_cfg=model_cfg["core_model"]["attn"],
-#                         )
-#                         for _ in range(model_cfg["core_model"]["num_layers"])
-#                     ]
-#                 ),
-#             }
-#         )
-
-#     def forward(self, x):
-#         """
-#         Pass an input through the model
-#         Args:
-#             x: torch.tensor(B, S, H)
-#         Returns:
-#             x: torch.tensor(B, S, H)
-#         """
-
-#         # apply dropout
-#         x = self.transformer.drop(x)
-
-#         # pass through the transformer blocks
-#         for block in self.transformer.h:
-#             x = block(x)
-
-#         return x
+        # now strictly share the c_proj weights w/o lora
+        for i in range(1 + self.k_interior_layers, len(self.transformer.h) - self.k_interior_layers):
+            base_cproj = self.transformer.h[1 + self.k_interior_layers].attn.c_proj
+            shared_cproj_weights = {name: module.weight for name, module in base_cproj.named_modules() if isinstance(module, nn.Linear)}
+            target_cproj = self.transformer.h[i].attn.c_proj
+            for name, module in target_cproj.named_modules():
+                if isinstance(module, nn.Linear):
+                    module.weight = shared_cproj_weights[name]
