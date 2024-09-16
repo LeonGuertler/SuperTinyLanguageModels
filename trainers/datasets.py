a27c4350ad6ef14226e04bb3a6a4e1acc5cd8ffc
guertlerlo <lg11@outlook.com>
2024-09-16 13:06:29 +0800

    revert to rndm idx in dataset

--- trainers/datasets.py
+++ trainers/datasets.py
@@ -72,19 +72,15 @@ class BaseDatasetRandom(DatasetInterface):
         """
         Get a batch of random data points in an infinite loop.
         """
-        while True:
             # Get a random index
-            #idx = random.randint(0, self.dataset_len - 1) ## TODO: shouldn't be necessary if it is already 
-            # shuffled at the document level in the load_data function
-
-            for idx in range(self.dataset_len):
+            idx = random.randint(0, self.dataset_len - 1) 
+        
+            # Extract a slice of data for x and y
+            x = torch.from_numpy((self.data[idx: idx + self.context_window]).astype(np.int64))
+            y = torch.from_numpy((self.data[idx + 1: idx + 1 + self.context_window]).astype(np.int64))
             
-                # Extract a slice of data for x and y
-                x = torch.from_numpy((self.data[idx: idx + self.context_window]).astype(np.int64))
-                y = torch.from_numpy((self.data[idx + 1: idx + 1 + self.context_window]).astype(np.int64))
-                
-                # Yield the data points
-                yield x, y
+            # Yield the data points
+            yield x, y
 
 
 class BytePoolingDataset(DatasetInterface):
