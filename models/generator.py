1a3585f3852e2ec062d2e5bcb05338d61f536ef4
bobbycxy <bobbycxy1994@gmail.com>
2024-09-16 12:02:52 +0800

    added entropy based temperature sampling
    revised the standardgenerator to work with single and batched logits

--- models/generator.py
+++ models/generator.py
@@ -5,6 +5,112 @@ Generator Base Wrapper
 import torch
 import torch.nn.functional as F
 
+## libraries for entropy calculation
+import math
+
+
+class EntropyTemperatureGenerator(torch.nn.Module):
+    '''
+    From: https://arxiv.org/pdf/2403.14541
+    Entropy based temepraure adjusts the temperature based on the entropy of the logits.
+    If logits highly uncertain, entropy is high, temperature is increased.
+    If logits are certain, entropy is low, temperature is decreased.
+    '''
+
+    def __init__(self, model, generate_cfg, device="cuda"):
+        super().__init__()
+        self.model = model
+        self.device = device
+        self.model = self.model.to(torch.device(self.device))
+        self.generate_config = generate_cfg
+
+    def default_generate(self, input_text):
+        '''
+        Generate text using the default generation method
+        '''
+        return self.generate(
+            input_text,
+            self.generate_config["max_new_tokens"],
+            self.generate_config["temperature"],
+            self.generate_config["top_k"],
+            self.generate_config.get("temperature_scaling_factor", 0.1)
+        )
+    
+    @torch.no_grad()
+    def generate(self, input_text, max_new_tokens, temperature_ceiling, top_k, temperature_scaling_factor):
+        """
+        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+        the sequence max_new_tokens times, feeding the predictions back into the model each time.
+        Most likely you'll want to make sure to be in model.eval() mode of operation for this.
+        """
+        idx = self.model.embedding_model.tokenize_input(
+            input_string=input_text,
+            add_eot=False,
+            truncate=True
+        )
+        # push to device
+        idx = torch.tensor(idx).unsqueeze(0).to(torch.device(self.device))
+        for _ in range(max_new_tokens):
+            # forward the model to get the logits for the index in the sequence
+            logits, _ = self.model.inference(idx)
+            # calculate the entropy of the logits
+            entropy = self.calculate_entropy(F.softmax(logits[-1], dim=-1))
+            # calculate the temperatures
+            temperature = temperature_ceiling * (0.8 ** (temperature_scaling_factor / entropy)) # 0.8 is fixed based on the paper
+            # pluck the logits at the final step and scale by desired temperature
+            logits = logits / temperature
+            # logits might have shape (b,t,v) or (t,v)
+            # optionally crop the logits to only the top k options
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                ## for batched logits
+                if len(logits.shape) == 3:
+                    logits[logits < v[:, :, [-1]]] = -float("Inf")
+                ## for single logits
+                else:
+                    logits[logits < v[:, [-1]]] = -float("Inf")
+
+            # apply softmax to convert logits to (normalized) probabilities
+            probs = torch.nn.functional.softmax(logits, dim=-1)
+            
+            idx_next = torch.multinomial(probs, num_samples=1)
+
+            # check if done
+            if idx_next == self.model.embedding_model.eot_token:
+                break
+
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return self.model.embedding_model.decode(idx.tolist())
+    
+    def calculate_entropy(self, probabilities):
+        """
+        Calculate the entropy of a probability distribution from softmaxed logits.
+        
+        :param probabilities: A PyTorch tensor of softmaxed logits
+        :return: The entropy value as a PyTorch tensor
+        """
+       
+        # Ensure the input is a PyTorch tensor
+        if not isinstance(probabilities, torch.Tensor):
+            probabilities = torch.tensor(probabilities)
+        
+        # Ensure the tensor is float for numerical stability
+        probabilities = probabilities.float()
+        
+        return -torch.sum(probabilities * torch.log(probabilities), dim=-1)
+
+
+    def forward(self, x):
+        """Call the underlying model"""
+        return self.model(x)
+
+    def embed(self, x):
+        """Embed the input"""
+        return self.model.embed(x)
+
+
+
 class BeamSearchGenerator(torch.nn.Module):
     def __init__(self, model, generate_cfg, device="cuda"):
         super().__init__()
@@ -90,9 +196,6 @@ class BeamSearchGenerator(torch.nn.Module):
         logits[0, unique_tokens] /= penalty ** counts.float()
 
 
-
-
-
 class StandardGenerator(torch.nn.Module):
     """Standard Generator Wrapper for GPT models"""
 
@@ -134,11 +237,16 @@ class StandardGenerator(torch.nn.Module):
             logits, _ = self.model.inference(idx)
             # pluck the logits at the final step and scale by desired temperature
             logits = logits / temperature
-            # logits have shape (b,t,v)
+            # logits might have shape (b,t,v) or (t,v)
             # optionally crop the logits to only the top k options
             if top_k is not None:
                 v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
-                logits[logits < v[:, [-1]]] = -float("Inf") # logits[logits < v[:, :, [-1]]] = -float("Inf")
+                ## for batched logits
+                if len(logits.shape) == 3:
+                    logits[logits < v[:, :, [-1]]] = -float("Inf")
+                ## for single logits
+                else:
+                    logits[logits < v[:, [-1]]] = -float("Inf")
 
             # apply softmax to convert logits to (normalized) probabilities
             probs = torch.nn.functional.softmax(logits, dim=-1)
@@ -165,6 +273,7 @@ class StandardGenerator(torch.nn.Module):
 GENERATOR_DICT = {
     "standard": lambda model, generate_cfg, device: StandardGenerator(model=model, generate_cfg=generate_cfg, device=device), 
     "beam_search": lambda model, generate_cfg, device: BeamSearchGenerator(model=model, generate_cfg=generate_cfg, device=device), 
+    "entropy_temperature": lambda model, generate_cfg, device: EntropyTemperatureGenerator(model=model, generate_cfg=generate_cfg, device=device)
     }
 
 def build_generator(model, generate_cfg, device):
