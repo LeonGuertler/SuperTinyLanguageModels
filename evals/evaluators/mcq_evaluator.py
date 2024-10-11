65629dfc11af9ef60f5ab7beeca09950b653022a
guertlerlo <lg11@outlook.com>
2024-10-11 17:22:03 +0800

    wip

--- evals/evaluators/mcq_evaluator.py
+++ evals/evaluators/mcq_evaluator.py
@@ -12,23 +12,17 @@ class MCQEvaluator(BaseEvaluator):
         yield_fn: Callable, 
         model_wrapper: BaseModelWrapper,
         yield_fn_params: Optional[Dict[str, Any]] = None,
-        eval_metric: Optional[str] = "Acc",
         eval_logging_path: Optional[str] = "MCQ",
     ):
         super().__init__()
-        """
-        TODO
-        """
-        self.eval_metric = eval_metric
+        """ TODO """
         self.eval_logging_path = eval_logging_path
         self.yield_fn = yield_fn(**yield_fn_params)
         self.model_wrapper = model_wrapper
     
 
     def evaluate(self, model):
-        """
-        TODO
-        """
+        """ TODO """
         model = self.model_wrapper(model) # wrap the model
 
         total, correct = 0, 0
@@ -43,7 +37,7 @@ class MCQEvaluator(BaseEvaluator):
             total += 1
         accuracy = correct / total if total > 0 else 0
         return {
-            f"{self.eval_logging_path}/{self.env_id}-{self.eval_metric}": accuracy
+            f"{self.eval_logging_path}/{self.env_id} (Acc.)": accuracy
             }
 
 
